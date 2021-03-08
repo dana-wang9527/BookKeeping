44c93e759eff4cf2477f25f1dfe20add04e32159
wangdana <wangdana1998@gmail.com>
2021-03-08 15:04:48 +0800

    修复刷新变成404页面的bug

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import clone from '@/lib/clone';
 import createId from '@/lib/idCreator';
+import router from '@/router';
 
 
 Vue.use(Vuex);
@@ -17,8 +18,37 @@ const newStore = new Vuex.Store({
         currentTag: undefined
     } as RootStore,
     mutations: {
-        setCurrentTag(state,id: string){
-            state.currentTag=state.tagList.filter(t => t.id === id)[0]
+        setCurrentTag(state, id: string) {
+            state.currentTag = state.tagList.filter(t => t.id === id)[0];
+        },
+        updateTag(state, {id, name}: { id: string; name: string }) {
+            const idList = state.tagList.map(item => item.id);
+            if (idList.indexOf(id) >= 0) {
+                const names = state.tagList.map(item => item.name);
+                if (names.indexOf(name) >= 0) {
+                    window.alert('标签名重复了');
+                } else {
+                    const tag = state.tagList.filter(item => item.id === id)[0];
+                    tag.name = name;
+                    newStore.commit('saveTag');
+                }
+            }
+        },
+        removeTag(state, id: string) {
+            let index = -1;
+            for (let i = 0; i < state.tagList.length; i++) {
+                if (state.tagList[i].id === id) {
+                    index = i;
+                    break;
+                }
+            }
+            if (index >= 0) {
+                state.tagList.splice(index, 1);
+                newStore.commit('saveTag');
+                router.back();
+            } else {
+                window.alert('删除失败');
+            }
         },
         fetchRecodes: function (state) {
             state.recodeList = JSON.parse(window.localStorage.getItem('recodeList') || '[]');
